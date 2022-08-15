8122ae4e0c6e51a30f9a659f8fdac2292a1bd510
Ninika Shchur <nikitatutspit@mail.ru>
2022-08-16 01:14:56 +0300

    Внесены изменения после ревью

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+
+const popup = document.querySelector('.popup');
+const profileName = document.querySelector('.profile__name');
+const profileWork = document.querySelector('.profile__work');
+const profileEdit = document.querySelector('.profile__edit-button');
+const popupForm = popup.querySelector('.popup__form')
+const popupName = popup.querySelector('.popup__form_name');
+const popupWork = popup.querySelector('.popup__form_work');
+const popupClose = popup.querySelector('.popup__close-button');
+const popupSave = popup.querySelector('.popup__save-button');
+
+
+function openPopup () {
+    popup.classList.toggle('popup_opened');
+}
+
+function closePopup () {
+    popup.classList.remove('popup_opened');
+    popupName.value = profileName.textContent;
+    popupWork.value = profileWork.textContent;
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = popupName.value;
+    profileWork.textContent = popupWork.value;
+}
+
+profileEdit.addEventListener('click', openPopup);
+popupClose.addEventListener('click', openPopup);
+popupSave.addEventListener('click', openPopup);
+popupForm.addEventListener('submit', formSubmitHandler); 
